6cebd288f6cde6c6ca80515fc1694230644ac266
yossybio <78083875+yossybio@users.noreply.github.com>
2023-11-05 12:36:13 +0000

    Set players page design complete

--- src/Pages/SetPlayers/index.jsx
+++ src/Pages/SetPlayers/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../../assets/SetPlayers.module.css";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import {
@@ -7,7 +8,9 @@ import {
 } from "../../Redux/gameDataSlice";
 import { Stack } from "@mui/material";
 import MainImg from "../../assets/MainPageImg.jpg";
-import style from "../../assets/SetPlayers.module.css";
+import { motion } from "framer-motion";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 export default function SetPlayersPage() {
   const navigate = useNavigate();
@@ -24,39 +27,63 @@ export default function SetPlayersPage() {
     dispatch(setPlayersMode(numOfPlayers));
     dispatch(setComputerPlayerSymbol({ computerPlayerSymbol: "O" }));
     savingDataInLocalStorage(numOfPlayers);
-    navigate("/setGrid");
+    setTimeout(() => { navigate("/setGrid"); }, 500);
+
   };
 
   return (
     <>
-      <Stack alignItems="center" spacing={8}>
-        <img
-          // style={{ width: "100vw", height: "40vh"}}
-          // src="https://thumbs.dreamstime.com/z/tic-tac-toe-1652844.jpg?w=992"
-          src={MainImg}
-          alt="Set players main img"
-        />
-        <Stack alignItems="center" spacing={3}>
-          <button
-            // style={{ direction: "rtl" }}
-            className={style.mainButton}
-            onClick={() => {
-              clickHandler({ numOfPlayers: USER_VS_CPU_MODE });
-            }}
-          >
-            שחקן נגד מחשב
-          </button>
-          <button
-            // style={{ direction: "rtl" }}
-            className={style.mainButton}
-            onClick={() => {
-              clickHandler({ numOfPlayers: USER_VS_USER_MODE });
-            }}
-          >
-            שחקן נגד שחקן
-          </button>
+      <body>
+        <Stack alignItems="center" spacing={8}>
+          <Box component="img"
+            src={MainImg} alt="Set players main img"
+            sx={{
+              height: "auto",
+              width: {
+                xs: "65%",
+                sm: "55%",
+                md: "45%",
+                lg: "30%",
+              }
+            }} />
+          <Stack alignItems="center" spacing={3}>
+            <Button
+              variant="outlined"
+              size="large"
+              onClick={() => {
+                clickHandler({ numOfPlayers: USER_VS_CPU_MODE });
+              }}
+              component={motion.div}
+              whileHover={{
+                backgroundColor: "white",
+                scale: 1.2,
+                transition: { duration: 0.3 }
+              }}
+              whileTap={{ scale: [1.5, 1.2], transition: { duration: 0.25 } }}
+              sx={{ backgroundColor: "#F9F7F7", color: "black" }}
+            >
+              שחקן נגד מחשב
+            </Button>
+            <Button
+              variant="outlined"
+              size="large"
+              onClick={() => {
+                clickHandler({ numOfPlayers: USER_VS_USER_MODE });
+              }}
+              component={motion.div}
+              whileHover={{
+                backgroundColor: "white",
+                scale: 1.2,
+                transition: { duration: 0.3 }
+              }}
+              whileTap={{ scale: [1.5, 1.2], transition: { duration: 0.25 } }}
+              sx={{ backgroundColor: "#F9F7F7", color: "black" }}
+            >
+              שחקן נגד שחקן
+            </Button>
+          </Stack>
         </Stack>
-      </Stack>
+      </body>
     </>
   );
 }
